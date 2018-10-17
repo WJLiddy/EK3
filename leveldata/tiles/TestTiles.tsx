e9e982facd906234beb48c259e1b0ca2517a9ee5
WJLIDDY <wjl36@case.edu>
2018-10-16 20:35:07 -0700

    some progress

---
+++ leveldata/tiles/TestTiles.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="TestTiles" tilewidth="19" tileheight="19" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="15" height="15" source="filler 1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="15" height="15" source="filler 2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="19" height="19" source="ledge end.png"/>
+ </tile>
+ <tile id="3">
+  <image width="15" height="15" source="slope down.png"/>
+ </tile>
+ <tile id="4">
+  <image width="15" height="15" source="slope up.png"/>
+ </tile>
+ <tile id="5">
+  <image width="15" height="15" source="top piece.png"/>
+ </tile>
+ <tile id="6">
+  <image width="15" height="15" source="spawn.png"/>
+ </tile>
+ <tile id="7">
+  <image width="15" height="15" source="en1.png"/>
+ </tile>
+</tileset>
